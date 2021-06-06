8b43d26485fd634cfd40fab7603f577ae07e3d68
François Lefebvre <francois.lfe@gmail.com>
2021-06-06 14:02:42 +0200

    chore(project): setup

--- server/services/firebase.ts
+++ server/services/firebase.ts
@@ -1,11 +1,8 @@
 import * as admin from 'firebase-admin';
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const serviceAccount = require('../../firebase.json');
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`
+  credential: admin.credential.applicationDefault(),
+  databaseURL: 'https://droplt.firebaseio.com'
 });
 
 export default admin;
